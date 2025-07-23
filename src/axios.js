534d91a514f13d8f15db6bf2249d96240ab372dd
Codingspy <praveenshinde2022mite@gmail.com>
2025-07-23 19:35:34 +0530

    added proper url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://adcampaignmvp-ayhtbcdxghfke7cd.canadacentral-01.azurewebsites.net/api', 
+  baseURL: 'https://adcampaignmvp-ayhtbcdxghfke7cd.canadacentral-01.azurewebsites.net', 
 });
 
 export default instance;
